2ac5681b0736fdc5092d6a6fc1177261403ef9d3
yeonju123 <43317876+yeonju123@users.noreply.github.com>
2019-08-19 14:45:35 -0400

    tempfile
    save intermediate files as tempfiles and remove them

--- convert.py
+++ convert.py
@@ -7,6 +7,7 @@ import logging
 import os
 import pynini
 import subprocess
+import tempfile
 import typing
 
 
@@ -16,8 +17,9 @@ def main(args: argparse.Namespace) -> None:
             far_reader.arc_type(), encode_labels=True
         )
         fst_key = 1
+        _, tmppath_ngram = tempfile.mkstemp(text=True)
         with pynini.Far(
-            "ngramdata.far",
+            tmppath_ngram,
             mode="w",
             arc_type=far_reader.arc_type(),
             far_type="default",
@@ -30,36 +32,40 @@ def main(args: argparse.Namespace) -> None:
                 assert fst.verify(), "FST is ill-formed"
                 far_reader.next()
                 fst_key += 1
-
-        model_name = "model.lm"
+        _, tmppath_cnt = tempfile.mkstemp(text=True)
+        _, tmppath_lm = tempfile.mkstemp(text=True)
+        logging.info(
+            "alignment.far is encoded to FSAs for training. Now training starts." )
         cmd = [
             "ngramcount",
             "--require_symbols=false",
             f"--order={args.ngram}",
-            "ngramdata.far",
-            "lm.cnt",
+            tmppath_ngram,
+            tmppath_cnt,
         ]
         subprocess.check_call(cmd)
         cmd1 = [
             "ngrammake",
             f"--method={args.smoothing}",
-            "lm.cnt",
-            model_name,
+            tmppath_cnt,
+            tmppath_lm,
         ]
         subprocess.check_call(cmd1)
-        logging.info(
-            "%s-gram %s Language model is built", args.ngram, args.smoothing
-        )
         if args.ngramshrink:
+            _, tmppath_lm_sh = tempfile.mkstemp(text=True)
             cmd = [
                 "ngramshrink",
-                "--method-relative_entropy",
+                "--method=relative_entropy",
                 "--target_number_of_ngrams=100000",
-                "model.lm",
-                "model.shrunk.lm",
+                tmppath_lm,
+                tmppath_lm_sh,
             ]
-            model_name = "model.shrunk.lm"
-        with pynini.Far(model_name, mode="r") as far_fsa_reader:
+            subprocess.check_call(cmd)
+            tmppath_lm = tmppath_lm_sh
+        logging.info(
+            "%s-gram %s Language model is trained.", args.ngram, args.smoothing
+        )
+        with pynini.Far(tmppath_lm, mode="r") as far_fsa_reader:
             fst_key = 1
             with pynini.Far(
                 args.output_path,
@@ -75,13 +81,12 @@ def main(args: argparse.Namespace) -> None:
                     far_writer.add(key, fst)
                     far_fsa_reader.next()
                     fst_key += 1
-    os.remove("ngramdata.far")
-    os.remove("lm.cnt")
-    os.remove("model.lm")
-    if args.ngramshrink:
-        os.remove("model.shrunk.lm")
-
-# write a function to delete all the temporal files. -> delet lm.cnt. and if ngramshrink, delete model.lm
+    os.remove(tmppath_ngram)
+    os.remove(tmppath_cnt)
+    os.remove(tmppath_lm)
+    logging.info(
+            "%s-gram %s Language model is built.", args.ngram, args.smoothing
+        )
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s", level="INFO")
@@ -96,7 +101,7 @@ if __name__ == "__main__":
         "--ngram", required=True, help="input the order of ngram"
     )
     parser.add_argument(
-        "--smoothing", required=True, help="input smoothing method"
+        "--smoothing", default="kneser_ney", help="input smoothing method (default: %(default)s)"
     )
     parser.add_argument(
         "--ngramshrink", action="store_true", help="make a compact ngram model"
