e84799dfe94cc814eaef42c87236ffcb5fd95ccf
kylebgorman <kylebgorman@gmail.com>
2019-08-20 10:26:22 -0400

    Fixes bug in CG construction.
    Modifies CG construction so that we compute:
    
            (G x P)*
    
    instead of
    
            G* x P*
    
    Also:
    
    - improves informativity of logging
    - adds check that the resulting FST is single-state
    - simplifies epsilon-handling slightly

--- covering.py
+++ covering.py
@@ -4,59 +4,62 @@
 
 import argparse
 import logging
-import pynini
+
 from typing import Set
 
 
+import pynini
+
+
 def _get_labels(far_path: str) -> Set[int]:
+    """Collects all observed labels from an FSA."""
     labels: Set[int] = set()
     with pynini.Far(far_path, mode="r") as far_reader:
         while not far_reader.done():
             fsa = far_reader.get_fst()
-            label_extract = fsa.paths().ilabels()
-            labels.update(label_extract)
+            # Only looks at the input side, assuming an FSA.
+            labels.update(fsa.paths().ilabels())
             far_reader.next()
     return labels
 
 
-def _make_star(labels: Set[int], epsilon: bool) -> pynini.Fst:
-    lattice = pynini.Fst()
-    starting_state = lattice.add_state()
-    lattice.set_start(starting_state)
-    lattice.set_final(starting_state)
+def _make_union(labels: Set[int], epsilon: bool) -> pynini.Fst:
+    """Creates FSA over a union of the labels."""
     if epsilon:
-        lattice.add_arc(starting_state, pynini.Arc(0, 0, None, starting_state))
+        labels.add(0)
+    lattice = pynini.Fst()
+    src = lattice.add_state()
+    lattice.set_start(src)
+    dst = lattice.add_state()
     for label in labels:
-        lattice.add_arc(
-            starting_state, pynini.Arc(label, label, None, starting_state)
-        )
-    assert lattice.verify()
+        lattice.add_arc(src, pynini.Arc(label, label, None, dst))
+    lattice.set_final(dst)
+    assert lattice.verify(), "FST is ill-formed"
     return lattice
 
 
-def size(f: pynini.Fst) -> int:
-    """Computes the number of arcs and states in the FST."""
-    return sum(1 + f.num_arcs(state) for state in f.states())
+def _narcs(f: pynini.Fst) -> int:
+    """Computes the number of arcs in an FST."""
+    return sum(f.num_arcs(state) for state in f.states())
 
 
-def main(args):
+def main(args: argparse.Namespace) -> None:
     g_labels = _get_labels(args.g_far_path)
+    logging.info("%d unique graphemes", len(g_labels))
+    g_lattice = _make_union(g_labels, args.input_epsilon)
     p_labels = _get_labels(args.p_far_path)
-    g_lattice = _make_star(g_labels, args.input_epsilon)
-    p_lattice = _make_star(p_labels, args.output_epsilon)
-    covering = pynini.transducer(g_lattice, p_lattice)
-    assert covering.verify()
-    fst_size = size(covering)
-    logging.info(
-        "The number of arcs and states in the covering grammar is %d", fst_size
-    )
+    logging.info("%d unique phonemes", len(p_labels))
+    p_lattice = _make_union(p_labels, args.output_epsilon)
+    # The covering grammar is defined as the closure of the cross-product of
+    # the input and output labels, corresponding to a Markov order-0 model.
+    covering = pynini.transducer(g_lattice, p_lattice).closure().optimize()
+    assert covering.num_states() == 1, "FST is ill-formed"
+    logging.info("Covering grammar has %d arcs", _narcs(covering))
     covering.write(args.covering_path)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO, format="%(levelname)s: %(message)s"
-    )
+    logging.basicConfig(format="%(levelname)s: %(message)s", level="INFO")
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
         "--g_far_path", required=True, help="input grapheme FAR path"
@@ -67,12 +70,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--input_epsilon",
         default=True,
-        help="allows input graphemes to have a null alignment (default: %(default)s)"
+        help="allows input graphemes to have a null alignment (default: %(default)s)",
     )
     parser.add_argument(
         "--output_epsilon",
         default=True,
-        help="allows input phonemes to have a null alignment (default: %(default)s)"
+        help="allows input phonemes to have a null alignment (default: %(default)s)",
     )
     parser.add_argument(
         "--covering_path", required=True, help="output covering FAR path"
