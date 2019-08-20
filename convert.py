e1c986a758f45510a655aeadd52c96551fa1af79
yeonju123 <43317876+yeonju123@users.noreply.github.com>
2019-08-20 12:42:14 -0400

    script improvement

--- convert.py
+++ convert.py
@@ -5,88 +5,79 @@
 import argparse
 import logging
 import os
-import pynini
 import subprocess
 import tempfile
-import typing
+
+
+import pynini
 
 
 def main(args: argparse.Namespace) -> None:
-    with pynini.Far(args.alignments_path, mode="r") as far_reader:
+    with pynini.Far(args.far_path, mode="r") as far_reader:
         encoder = pynini.EncodeMapper(
             far_reader.arc_type(), encode_labels=True
         )
-        fst_key = 1
-        _, tmppath_ngram = tempfile.mkstemp(text=True)
+        fsa_path = tempfile.mkstemp(text=True)[1]
         with pynini.Far(
-            tmppath_ngram,
+            fsa_path,
             mode="w",
             arc_type=far_reader.arc_type(),
             far_type="default",
         ) as far_writer:
             while not far_reader.done():
-                key = f"{fst_key:08x}"
                 fst = far_reader.get_fst()
-                fst.encode(encoder)
-                far_writer.add(key, fst)
                 assert fst.verify(), "FST is ill-formed"
+                fst.encode(encoder)
+                far_writer.add(far_reader.get_key(), fst)
                 far_reader.next()
-                fst_key += 1
-        _, tmppath_cnt = tempfile.mkstemp(text=True)
-        _, tmppath_lm = tempfile.mkstemp(text=True)
+        count_path = tempfile.mkstemp(text=True)[1]
+        lm_path = tempfile.mkstemp(text=True)[1]
         logging.info(
             "alignment.far is encoded to FSAs for training. Now training starts."
         )
         cmd = [
             "ngramcount",
             "--require_symbols=false",
-            f"--order={args.ngram}",
-            tmppath_ngram,
-            tmppath_cnt,
+            f"--order={args.order}",
+            fsa_path,
+            count_path,
         ]
         subprocess.check_call(cmd)
-        cmd = [
+        os.remove(fsa_path)
+        cmd1 = [
             "ngrammake",
-            f"--method={args.smoothing}",
-            tmppath_cnt,
-            tmppath_lm,
+            f"--method={args.smoothing_method}",
+            count_path,
+            lm_path,
         ]
-        subprocess.check_call(cmd)
-        if args.ngramshrink:
-            _, tmppath_lm_sh = tempfile.mkstemp(text=True)
+        subprocess.check_call(cmd1)
+        os.remove(count_path)
+        if args.shrinking_method:
+            shrunk_lm_sh = tempfile.mkstemp(text=True)[1]
             cmd = [
-                "ngramshrink",
+                "shrinking_method",
                 "--method=relative_entropy",
-                "--target_number_of_ngrams=100000",
-                tmppath_lm,
-                tmppath_lm_sh,
+                f"--target_number_of_ngrams={args.target_number_of_ngrams}",
+                lm_path,
+                shrunk_lm_sh,
             ]
             subprocess.check_call(cmd)
-            tmppath_lm = tmppath_lm_sh
+            lm_path = shrunk_lm_sh
         logging.info(
-            "%s-gram %s Language model is trained.", args.ngram, args.smoothing
+            "%s-gram %s Language model is trained.",
+            args.order,
+            args.smoothing_method,
         )
-        with pynini.Far(tmppath_lm, mode="r") as far_fsa_reader:
-            fst_key = 1
-            with pynini.Far(
-                args.output_path,
-                mode="w",
-                arc_type=far_fsa_reader.arc_type(),
-                far_type="default",
-            ) as far_writer:
-                while not far_fsa_reader.done():
-                    key = f"{fst_key:08x}"
-                    fst = far_fsa_reader.get_fst()
-                    assert fst.verify(), "FST is ill-formed"
-                    fst.decode(encoder)
-                    far_writer.add(key, fst)
-                    far_fsa_reader.next()
-                    fst_key += 1
-    os.remove(tmppath_ngram)
-    os.remove(tmppath_cnt)
-    os.remove(tmppath_lm)
+        # Decoding the LM
+        model = pynini.Fst.read(lm_path)
+        os.remove(lm_path)
+        model.decode(encoder)
+        model.write(args.model_path)
+
     logging.info(
-        "%s-gram %s Language model is built.", args.ngram, args.smoothing
+        "%s-gram %s Language model is built.",
+        args.order,
+        args.smoothing_method,
     )
 
 
@@ -94,20 +85,28 @@ if __name__ == "__main__":
     logging.basicConfig(format="%(levelname)s: %(message)s", level="INFO")
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
-        "--alignments_path", required=True, help="input alignments FAR path"
+        "--far_path", required=True, help="input alignments FAR path"
     )
     parser.add_argument(
-        "--output_path", required=True, help="input result FST path"
+        "--model_path", required=True, help="input result FST path"
     )
     parser.add_argument(
-        "--ngram", required=True, help="input the order of ngram"
+        "--order", required=True, help="input the order of ngram"
     )
     parser.add_argument(
-        "--smoothing",
+        "--target_number_of_ngrams",
+        default=100000,
+        help="input the target number of ngrams (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--smoothing_method",
         default="kneser_ney",
         help="input smoothing method (default: %(default)s)",
     )
     parser.add_argument(
-        "--ngramshrink", action="store_true", help="make a compact ngram model"
+        "--shrinking_method",
+        action="store_true",
+        help="make a compact ngram model",
     )
+
     main(parser.parse_args())
