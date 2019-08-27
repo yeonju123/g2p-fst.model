ef987af7fa3f9ff7044505a3791e8f929327b4c2
yeonju123 <43317876+yeonju123@users.noreply.github.com>
2019-08-27 18:32:49 -0400

    edited delimiter from "/t" to ", "
    data_merge.py merges gold data and prediction by ",". evaluate.py, taking the product of data_merge.py as its argument, is edited accordingly.

---
+++ Automazing_g2p_experiment/evaluate.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+"""Evaluates sequence model.
+
+This script assumes the gold and hypothesis data is stored in a two-column TSV
+file, one example per line."""
+
+
+import argparse
+import logging
+import multiprocessing
+import numpy
+
+from typing import Any, Iterator, List, Tuple
+
+
+Labels = List[Any]
+
+
+def _edit_distance(x: Labels, y: Labels) -> int:
+    # For a more expressive version of the same, see:
+    #
+    #     https://gist.github.com/kylebgorman/8034009
+    idim = len(x) + 1
+    jdim = len(y) + 1
+    table = numpy.zeros((idim, jdim), dtype=numpy.uint8)
+    table[1:, 0] = 1
+    table[0, 1:] = 1
+    for i in range(1, idim):
+        for j in range(1, jdim):
+            if x[i - 1] == y[j - 1]:
+                table[i][j] = table[i - 1][j - 1]
+            else:
+                c1 = table[i - 1][j]
+                c2 = table[i][j - 1]
+                c3 = table[i - 1][j - 1]
+                table[i][j] = min(c1, c2, c3) + 1
+    return int(table[-1][-1])
+
+
+def _score(gold: str, hypo: str) -> Tuple[int, int]:
+    """Computes sufficient statistics for LER calculation."""
+    gold_labels = list(gold)
+    hypo_labels = list(hypo)
+    edits = _edit_distance(gold_labels, hypo_labels)
+    if edits:
+        logging.warning(
+            "Incorrect prediction:\t%s (predicted: %s)", gold, hypo
+        )
+    return (edits, len(gold_labels))
+
+
+def _tsv_reader(path: str) -> Iterator[Tuple[str, str]]:
+    """Reads pairs of strings from a TSV filepath."""
+    with open(path, "r") as source:
+        for line in source:
+            (gold, hypo) = line.rstrip().split(",", 1)
+            yield (gold, hypo)
+
+
+def main(args: argparse.Namespace) -> None:
+    # Word-level measures.
+    correct = 0
+    incorrect = 0
+    # Label-level measures.
+    total_edits = 0
+    total_length = 0
+    # Since the edit distance algorithm is quadratic, let's do this with
+    # multiprocessing.
+    with multiprocessing.Pool() as pool:
+        gen = pool.starmap(_score, _tsv_reader(args.tsv_path))
+        for (edits, length) in gen:
+            if edits == 0:
+                correct += 1
+            else:
+                incorrect += 1
+            total_edits += edits
+            total_length += length
+    logging.info(f"{args.tsv_path} WER:\t%.4f", incorrect / (correct + incorrect))
+    logging.info(f"{args.tsv_path} LER:\t%.4f", total_edits / total_length)
+    #print("f{args.tsv_path} WER:\t%.4f", incorrect / (correct + incorrect))
+    #print("f{args.tsv_path} LER:\t%.4f", total_edits / total_length)
+    
+    
+if __name__ == "__main__":
+    logging.basicConfig(filename = "log.txt", level="INFO", format="%(levelname)s: %(message)s")
+    parser = argparse.ArgumentParser(description="Evaluates sequence model")
+    parser.add_argument("tsv_path", help="path to gold/hypo TSV file")
+    main(parser.parse_args())
+
