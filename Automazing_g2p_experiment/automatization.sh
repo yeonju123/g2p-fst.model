fd999feb0791d9797853208ab4d5c30f1498f4fa
yeonju123 <43317876+yeonju123@users.noreply.github.com>
2019-08-27 18:26:07 -0400

    fixed bug

--- Automazing_g2p_experiment/automatization.sh
+++ Automazing_g2p_experiment/automatization.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-seed="100"
+
+set -eou pipefail
+
+
 lang="hi"
-orders="6 7 8"
+seed="100"
+orders="3 4 5 6 7 8"
 smoothing_methods="kneser_ney witten_bell absolute katz presmoothed unsmoothed"
 
 
 data_split () {
-        python g2p_specific_data_split.py --seed $seed --input_path ${1}.tsv --train_path ${1}_train.tsv --g_dev_path ${1}_g_dev.tsv --p_dev_path ${1}_p_dev.tsv --g_test_path ${1}_g_test.tsv --test_p_path ${1}_p_test.tsv
+        python g2p_specific_data_split.py --seed ${1} --input_path ${2}.tsv --train_path ${2}_train.tsv --g_dev_path ${2}_g_dev.tsv --p_dev_path ${2}_p_dev.tsv --g_test_path ${2}_g_test.tsv --p_test_path ${2}_p_test.tsv
 }
 
 train_rewrite () {
         #train pairngram model
-        python train.py --input_path ${lang}_train --order $1 --smoothing_method $2 --model_path ${lang}_${1}_${2}_model.fst
-        model_name="${lang}_${1}_${2}_model.fst"
+        python train.py --input_path ${lang}_train.tsv --order $1 --smoothing_method $2 --model_path ${lang}_${1}_${2}_model.fst
+        model_name="${lang}_${1}_${2}"
         }
 
 predict_merge_eval (){
 	#predict
-        python rewriter.py --word_path ${lang}_g_${1}.tsv --rule_path $model_name --token_type utf8 > predict.tsv       
-	python data_merge.py ${lang}_p_${1}.tsv predict.tsv eval.tsv
+        python rewrite.py --word_path ${lang}_g_${1}.tsv --fst_path ${model_name}_model.fst --token_type utf8 > predict.tsv       
+	python data_merge.py ${lang}_p_${1}.tsv predict.tsv ${model_name}_eval.tsv
 	#evaluate word/label error rate
-        python evaluate.py eval.tsv >> ${lang}_result.txt
+        python evaluate.py ${model_name}_eval.tsv
 }
 
-
-data_split $lang
+data_split $seed $lang
 touch ${lang}_result.txt
 
 for order in $orders; do
@@ -34,3 +37,5 @@ for order in $orders; do
 		predict_merge_eval dev
 done
 done
+#write a code to delete all the unnecessary files. 
+
